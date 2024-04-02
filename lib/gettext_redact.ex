1fce86f277fcd2e88346b3608cd5a1aaa4c6e816
mark olson <theothermarkolson@gmail.com>
2024-04-01 20:31:03 -0400

    start sketching things out a bit

--- lib/gettext_redact.ex
+++ lib/gettext_redact.ex
@@ -1,18 +1,42 @@
 defmodule GettextRedact do
-  @moduledoc """
-  Documentation for `GettextRedact`.
-  """
+  @doc "Erase your gettext"
 
-  @doc """
-  Hello world.
+  def process_string(%Expo.Message.Singular{} = msg) do
+    text = redact(msg.msgstr)
+  end
 
-  ## Examples
+  def process_string(%Expo.Message.Plural{} = msg) do
+    msg
+  end
 
-      iex> GettextRedact.hello()
-      :world
+  # Going to need to make string slices for interpolations
+  # and re-join at the end. For now just wipe it all.
 
-  """
-  def hello do
-    :world
+  def redact(text) do
+    String.split(text, "", trim: true)
+    |> Enum.reduce([], fn char, acc ->
+      char = if skip_redacting?(char), do: char, else: erase()
+      [char | acc]
+    end)
+    |> IO.inspect()
+    |> Enum.reverse()
+    |> Enum.join()
+  end
+
+  defp pot_path do
+    Application.get_env(:gettext_redact, :path, ["priv/gettext"])
+  end
+
+  defp erase do
+    options = Application.get_env(:gettext_redact, :eraser, "█") |> List.wrap()
+
+    case length(options) do
+      1 -> hd(options)
+      _ -> Enum.random(options)
+    end
+  end
+
+  defp skip_redacting?(char) do
+    char in List.wrap(Application.get_env(:gettext_redact, :skip, [" ", ", ", "!"]))
   end
 end
